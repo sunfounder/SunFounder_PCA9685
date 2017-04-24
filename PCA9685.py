a0663eb102828c52068b416888fd54fa80fab869
Cavon <lijiarong@sunfounder.com>
2017-04-24 13:36:56 +0800

    Do not close program if i2c failed

--- PCA9685.py
+++ PCA9685.py
@@ -183,8 +183,7 @@ class PWM(object):
         else:
             print "Device is missing."
             print "Check the address or wiring of PCA9685 Server driver, or email this information to support@sunfounder.com"
-            print 'exiting...'
-        quit()
+        raise IOError('IO error')
 
     @property
     def frequency(self):
